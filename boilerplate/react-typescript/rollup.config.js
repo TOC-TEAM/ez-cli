d07d83eeb8bf8dfa9758feda5c59db7988b6c8bc
chenxianlong <794465731@qq.com>
2019-03-14 21:08:34 +0800

    improve: typescript boilerplate and get boilerplates dynamically

--- boilerplate/react-typescript/rollup.config.js
+++ boilerplate/react-typescript/rollup.config.js
@@ -1,11 +1,14 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss-modules'
 import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
 
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
@@ -21,6 +24,12 @@ export default {
     },
   ],
   plugins: [
+    external(),
+    postcss({
+      modules: true,
+      writeDefinitions: true,
+    }),
+    url(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
