f504a076d16ed05baabd08c98ed4ae27644649c1
chenxianlong <794465731@qq.com>
2019-04-01 23:00:07 +0800

    test: mockReturnedValue of ora

--- __mocks__/ora.js
+++ __mocks__/ora.js
@@ -2,4 +2,10 @@ const ora = jest.genMockFromModule('ora')
 
 ora.promise = () => true
 
+ora.mockReturnValue({
+  start: text => text,
+  succeed: text => text,
+  fail: text => text,
+})
+
 module.exports = ora
